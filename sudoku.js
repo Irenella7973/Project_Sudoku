6d2aabbf477b2f0a9cd23f86ac59ad38e248c7f6
Irina Tikhonova <102436907+Irenella7973@users.noreply.github.com>
2022-05-20 12:07:51 +0300

    Create func returnStr

--- sudoku.js
+++ sudoku.js
@@ -1,32 +1,23 @@
-// Takes a board as a string in the format
-// you see in the puzzle file. Returns
-// something representing a board after
-// your solver has tried to solve it.
-// How you represent your board is up to you!
-function solve(boardString) {
-  console.log('first changes');
+const fs = require('fs');
+// функция которая возвращает указанную строку в массиве массивов
+function returnStr(numStr) {
+  const text = fs.readFileSync('./sudoku-puzzles.txt', 'utf-8');
+  const allText = text.split('\n');
+  let str = allText[numStr];
+  const arr = [];
+  for (let i = 0; i < str.length; i++) {
+    if (i % 9 === 0) {
+      arr.push('\n');
+      arr.push(str[i]);
+    } else arr.push(str[i]);
+  }
+  str = arr.join('');
+  const result = [];
+  let arrStr = str.split('\n');
+  arrStr = arrStr.slice(1);
+  for (let i = 0; i < arrStr.length; i++) {
+    result.push(arrStr[i].split(''));
+  }
+  // console.table(result)
+  return result;
 }
-
-// Returns a boolean indicating whether
-// or not the provided board is solved.
-// The input board will be in whatever
-// form `solve` returns.
-function isSolved(board) {
-
-}
-
-// Takes in a board in some form and
-// returns a String that's well formatted
-// for output to the screen.
-// The input board will be in whatever
-// form `solve` returns.
-function prettyBoard(board) {
-
-}
-
-// Exports all the functions to use them in another file.
-module.exports = {
-  solve,
-  isSolved,
-  prettyBoard,
-};
